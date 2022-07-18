083fba17a379692451d119387c5e96b0c412d597
channchealy <sek.channchealy@yahoo.com>
2022-07-18 18:08:20 -0500

    feature/question

---
+++ index.js
@@ -0,0 +1,90 @@
+const { rejects } = require('assert');
+const fs = require('fs');
+const inquirer = require('inquirer');
+const { resolve } = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+const getPrompUser =  promptUser => {
+    return inquirer.prompt([
+        {
+            name: "Title",
+            message: "What is the title of your project?",
+            type:"input",
+        },
+        {
+            type:"input",
+            name: "Description",
+            message: "Give a short description of your project",
+            
+        },
+        {
+            type:"input",
+            name: "Installation",
+            message: "What instructions should users follow to install your application?",
+            
+        },
+        {
+            type:"input",
+            name: "Usage",
+            message: "How to use this applicaiton?",
+        },
+        {
+        type:"input",
+        name: "Contributing",
+        message: "How can users contribute to this project?",
+        },
+        {
+            type:"input",
+            name: "Tests",
+            message: "Give test instructions",
+        },
+
+        {
+            type: "list",
+            name: "licenseType",
+            message: "Chose the appropiate license for this project",
+            choices: [
+                "MIT License",
+                "GNU GPL v3",
+                "The Unlicense"
+            ]
+        },
+        {
+            type:"input",
+            name: "Questions",
+            message: "Enter your github username.",
+        },
+        {
+            type: 'input',
+            name: 'Email',
+            message: 'Enter your Email address',
+        }
+
+    ]);
+
+};
+
+const writeToFile = fileContents => {
+    return new Promise((resolve, rejects) =>{
+        fs.writeFile('README.md', fileContents, err => {
+            if(err){
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok:true,
+                message:'Successfully wrote to README.md!'
+            })
+        })
+    })
+};
+
+getPrompUser()
+    .then(promptUser => {
+        return generateMarkdown(promptUser);
+    })
+    .then(_getAnswers => {
+        writeToFile(_getAnswers);
+        console.log('Successfully wrote to README.md!');
+    }).catch((err) => console.error(err));
